a267043f27bf5599151cd5062db73b2a8ff6674f
Breno Santos <brenosantos145@gmail.com>
2023-10-10 21:48:20 -0300

    feat: adding more regex to validate path

--- controller/task.go
+++ controller/task.go
@@ -10,6 +10,8 @@ var (
 	listTaskRe   = regexp.MustCompile(`^\/task[\/]*$`)
 	getTaskRe    = regexp.MustCompile(`^\/task\/(\d+)$`)
 	createTaskRe = regexp.MustCompile(`^\/task[\/]*$`)
+	deleteTaskRe = regexp.MustCompile(`^\/task\/(\d+)$`)
+	updateTaskRe = regexp.MustCompile(`^\/task\/(\d+)$`)
 )
 
 type TaskHandler struct{}
@@ -23,6 +25,10 @@ func (h *TaskHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "GET")
 	case r.Method == "POST" && createTaskRe.MatchString(r.URL.Path):
 		fmt.Fprintln(w, "POST")
+	case r.Method == "DELETE" && deleteTaskRe.MatchString(r.URL.Path):
+		fmt.Fprintln(w, "DELETE")
+	case r.Method == "PUT" && updateTaskRe.MatchString(r.URL.Path):
+		fmt.Fprintln(w, "PUT")
 	default:
 		http.NotFound(w, r)
 	}
