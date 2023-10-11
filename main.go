df0d40868d86d026924fde05a3846d9fcf1f5b6e
Breno Santos <brenosantos145@gmail.com>
2023-10-10 21:10:01 -0300

    feat: adding switch to methods

--- main.go
+++ main.go
@@ -8,7 +8,19 @@ import (
 type TaskHandler struct{}
 
 func (h *TaskHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	w.Header().Set("content-type", "application/json")
+	switch {
+	case r.Method == "GET":
+		fmt.Fprintln(w, "GET")
+	case r.Method == "POST":
+		fmt.Fprintln(w, "POST")
+	case r.Method == "PUT":
+		fmt.Fprintln(w, "PUT")
+	case r.Method == "DELETE":
+		fmt.Fprintln(w, "DELETE")
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func main() {
