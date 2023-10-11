937de581eb54c1700aae9b89518d0240c6f18e1a
Breno Santos <brenosantos145@gmail.com>
2023-10-10 21:06:13 -0300

    feat: initing the mux server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type TaskHandler struct{}
+
+func (h *TaskHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	mux.Handle("/task/", &TaskHandler{})
+
+	fmt.Println("Server is running on port 3001")
+	http.ListenAndServe(":3001", mux)
 }
